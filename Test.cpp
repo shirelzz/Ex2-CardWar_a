3f7632b936bd3863fdf26e1b5d142f69e397950d
shirelzz <shirel.zz2016@gmail.com>
2023-03-27 19:32:39 +0300

    add test case

--- Test.cpp
+++ Test.cpp
@@ -6,10 +6,10 @@ using namespace std;
 #include <stdexcept>
 #include <iostream>
 
-TEST_CASE("Same name at initialization"){
+TEST_CASE("Same player is in the same game"){
     Player player("Avi");
 
-    CHECK_THROWS(Game{player,player});
+    CHECK_THROWS(Game(player,player));
 }
 
 TEST_CASE("Same player is in more than one game"){
@@ -48,6 +48,7 @@ TEST_CASE("number of cards at the end of the game"){
     CHECK(player2.stacksize() <= 52);
     CHECK(player2.stacksize() >= 0);
     CHECK(player1.cardesTaken() + player2.cardesTaken() == 26);
+    CHECK_FALSE( !(player1.cardesTaken() < player2.cardesTaken() || player1.cardesTaken() > player2.cardesTaken()) );
 
 }
 
@@ -106,6 +107,61 @@ TEST_CASE("number of cards left in the stack in each turn"){
     for(int i = 0; i<25; i++){
         game.playTurn();
         CHECK_EQ(player1.stacksize(), player2.stacksize());
+    }
+
+}
+
+
+TEST_CASE("checks if not possible to play when one of them has no cards"){
+    Player player1("Avi");
+    Player player2("Eli");
+
+    Game game(player1, player2);
+
+    for(int i = 0; i<25; i++){
+        if (player1.stacksize() > 0 && player2.stacksize()>0)
+        {
+            game.playTurn();
+        }
+        else{
+            CHECK_THROWS(game.playTurn());
+        }
+    }
+
+} 
+
+
+TEST_CASE("case of a tie"){
+    Player player1("Avi");
+    Player player2("Eli");
+
+    Game game(player1, player2);
+
+    for(int i = 0; player1.stacksize() == 0 && player2.stacksize() == 0  && i<1000 ; i++){
+
+        //if there is a winner
+        if (((player1.cardesTaken() > player2.cardesTaken() || player1.cardesTaken() < player2.cardesTaken()) &&
+            player1.stacksize() == 0 && player2.stacksize() == 0 ) ||
+            i==999)
+        {
+            std::cout << "Didn't reach the purpose of the test." << endl;
+            break;
+        }
+        
+        //if there is no winner yet
+        game.playTurn();
+
+        //if there's a tie
+        if (player1.stacksize() == 0 &&
+            player2.stacksize() == 0 &&
+            player1.cardesTaken() == player2.cardesTaken())
+        {
+            CHECK_THROWS(game.playTurn()); //should take all the cards thrown and divide them evenly to both players, and then they can keep playing
+            CHECK(player1.stacksize() >= 0);
+            CHECK(player2.stacksize() >= 0);
+            CHECK_EQ(player1.stacksize(), player2.stacksize());
+        }
+
     }
 
 } 
